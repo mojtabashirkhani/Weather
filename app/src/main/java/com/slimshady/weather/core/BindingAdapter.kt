a6c338ea05e8b5e872796138ba0a74145661c8d0
mojtabashirkhani <mojtabashirkhani.nba@gmail.com>
2021-12-19 22:31:12 +0330

    add weather icons

--- app/src/main/java/com/slimshady/weather/core/BindingAdapter.kt
+++ app/src/main/java/com/slimshady/weather/core/BindingAdapter.kt
@@ -32,6 +32,7 @@ fun setWeatherIcon(view: ImageView, iconPath: String?) {
     if (iconPath.isNullOrEmpty())
         return
     Picasso.get().cancelRequest(view)
+
     val newPath = iconPath.replace(iconPath, "a$iconPath")
     val imageid = view.context.resources.getIdentifier(newPath + "_svg", "drawable", view.context.packageName)
     val imageDrawable = view.context.resources.getDrawable(imageid, null)
