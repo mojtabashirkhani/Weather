c21f27eff13ebd5e2ba7620ef6b5e43a23b4d5d7
mojtabashirkhani <mojtabashirkhani.nba@gmail.com>
2022-04-23 13:47:08 +0430

    picasso bug fix

--- app/src/main/java/com/slimshady/weather/core/App.kt
+++ app/src/main/java/com/slimshady/weather/core/App.kt
@@ -4,11 +4,18 @@ package com.slimshady.weather.core
 import android.content.Context
 import androidx.multidex.MultiDex
 import com.slimshady.weather.di.component.DaggerCoreComponent
+import com.squareup.picasso.Picasso
 import dagger.android.AndroidInjector
 import dagger.android.support.DaggerApplication
 
 
 class App : DaggerApplication() {
+
+    override fun onCreate() {
+        super.onCreate();
+        Picasso.setSingletonInstance(Picasso.Builder(this).build())
+
+    }
 
 
     override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
