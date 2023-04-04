f2c20fbda0dd8dd6b90338d15f0e5391920aff42
Mojtaba Shirkhani <60153@sap.local>
2023-04-04 13:02:54 +0430

    fully tested [CurrentWeatherDaoTest]

--- app/src/test/java/com/slimshady/dao/CurrentWeatherDaoTest.kt
+++ app/src/test/java/com/slimshady/dao/CurrentWeatherDaoTest.kt
@@ -6,6 +6,8 @@ import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.google.common.truth.Truth
+import com.slimshady.util.generateCurrentWeatherEntity
+import com.slimshady.util.getOrAwaitValue
 import com.slimshady.weather.data.local.db.AppDatabase
 import com.slimshady.weather.data.local.db.dao.CurrentWeatherDao
 import org.junit.After
@@ -52,4 +54,50 @@ class CurrentWeatherDaoTest {
         Truth.assertThat(count).isEqualTo(0)
     }
 
+    @Test
+    fun `insert one entity and count must be one`() {
+        currentWeatherDao.insertCurrentWeather(generateCurrentWeatherEntity("Tehran", 1))
+
+        // When
+        val count = currentWeatherDao.getCount()
+
+        // Then
+        Truth.assertThat(count).isEqualTo(1)
+    }
+
+    @Test
+    fun `insert one entity and test get function`() {
+        currentWeatherDao.insertCurrentWeather(generateCurrentWeatherEntity("Tehran", 1))
+
+        val entity = currentWeatherDao.getCurrentWeather().getOrAwaitValue()
+        Truth.assertThat(entity.name).isEqualTo("Tehran")
+    }
+
+    @Test
+    fun `delete and insert a current weather`(){
+        // when
+        currentWeatherDao.deleteAndInsert(generateCurrentWeatherEntity("Tehran", 1))
+        val count = currentWeatherDao.getCount()
+        Truth.assertThat(count).isEqualTo(1)
+
+        // Then
+        currentWeatherDao.deleteAndInsert(generateCurrentWeatherEntity("Adana", 2))
+        val newCount = currentWeatherDao.getCount()
+        val value = currentWeatherDao.getCurrentWeather().getOrAwaitValue()
+        Truth.assertThat(newCount).isEqualTo(1)
+        Truth.assertThat(value.name).isEqualTo("Adana")
+    }
+
+    @Test
+    fun `first insert a current weather then delete and count must be zero`(){
+        // When
+        currentWeatherDao.deleteAndInsert(generateCurrentWeatherEntity("Tehran", 1))
+        val count = currentWeatherDao.getCount()
+        Truth.assertThat(count).isEqualTo(1)
+
+        // Then
+        currentWeatherDao.deleteCurrentWeather()
+        val newCount = currentWeatherDao.getCount()
+        Truth.assertThat(newCount).isEqualTo(0)
+    }
 }
